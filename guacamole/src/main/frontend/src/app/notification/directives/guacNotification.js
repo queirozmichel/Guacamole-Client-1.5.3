2e4033edfc212e6f8111563a146db30a6e30e63f
Michel <michelantunezqueiroz@gmail.com>
2024-10-04 15:19:22 -0300

    Removida a contagem regressiva para tentativa de reconexão.

--- guacamole/src/main/frontend/src/app/notification/directives/guacNotification.js
+++ guacamole/src/main/frontend/src/app/notification/directives/guacNotification.js
@@ -20,67 +20,36 @@
 /**
  * A directive for the guacamole client.
  */
-angular.module('notification').directive('guacNotification', [function guacNotification() {
-
+angular.module("notification").directive("guacNotification", [
+  function guacNotification() {
     return {
-        restrict: 'E',
-        replace: true,
-        scope: {
+      restrict: "E",
+      replace: true,
+      scope: {
+        /**
+         * The notification to display.
+         *
+         * @type Notification|Object
+         */
+        notification: "=",
+      },
 
-            /**
-             * The notification to display.
-             *
-             * @type Notification|Object 
-             */
-            notification : '='
+      templateUrl: "app/notification/templates/guacNotification.html",
+      controller: [
+        "$scope",
+        "$interval",
+        function guacNotificationController($scope, $interval) {
+          // Update progress bar if end known
+          $scope.$watch("notification.progress.ratio", function updateProgress(ratio) {
+            $scope.progressPercent = ratio * 100;
+          });
 
+          // Clean up interval upon destruction
+          $scope.$on("$destroy", function destroyNotification() {
+            if ($scope.interval) $interval.cancel($scope.interval);
+          });
         },
-
-        templateUrl: 'app/notification/templates/guacNotification.html',
-        controller: ['$scope', '$interval', function guacNotificationController($scope, $interval) {
-
-            // Update progress bar if end known
-            $scope.$watch("notification.progress.ratio", function updateProgress(ratio) {
-                $scope.progressPercent = ratio * 100;
-            });
-
-            $scope.$watch("notification", function resetTimeRemaining(notification) {
-
-                var countdown = notification.countdown;
-
-                // Clean up any existing interval
-                if ($scope.interval)
-                    $interval.cancel($scope.interval);
-
-                // Update and handle countdown, if provided
-                if (countdown) {
-
-                    $scope.timeRemaining = countdown.remaining;
-
-                    $scope.interval = $interval(function updateTimeRemaining() {
-
-                        // Update time remaining
-                        $scope.timeRemaining--;
-
-                        // Call countdown callback when time remaining expires
-                        if ($scope.timeRemaining === 0 && countdown.callback)
-                            countdown.callback();
-
-                    }, 1000, $scope.timeRemaining);
-
-                }
-
-            });
-
-            // Clean up interval upon destruction
-            $scope.$on("$destroy", function destroyNotification() {
-
-                if ($scope.interval)
-                    $interval.cancel($scope.interval);
-
-            });
-
-        }]
-
+      ],
     };
-}]);
+  },
+]);
